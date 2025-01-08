9f3246a82fba6aa4d0f0d4eaf11c776d5cca0dd8
SergioCantera <sgfcantera@hotmail.com>
2025-01-08 15:22:37 +0100

    Change author photo

--- src/components/PostBody.tsx
+++ src/components/PostBody.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown'
 
 const components: {} = {
   p: (p: { children: string }) => {
-    return <p className="my-6 text-lg">{p.children}</p>
+    return <p className="my-6 text-xl">{p.children}</p>
   },
   a: (a: { children: string; href: string }) => {
     return (
@@ -15,11 +15,17 @@ const components: {} = {
       </a>
     )
   },
+  li: (li: {children:string}) => {
+    return <li className='block ml-8 list-disc text-xl'>{ li.children }</li>
+  },
+  code: (code: { children:string }) => {
+    return <code className="inline-block text-lg px-1 py-2 text-[#232629] bg-[#e3e6e8] rounded-sm italic">{code.children}</code>
+  },
   h2: (h2: { children: string }) => {
-    return <h2 className="text-3xl">{h2.children}</h2>
+    return <h2 className="text-4xl">{h2.children}</h2>
   },
   h3: (h3: { children: string }) => {
-    return <h3 className="text-2xl">{h3.children}</h3>
+    return <h3 className="text-3xl font-semibold">{h3.children}</h3>
   },
   img: (img: { src: string; alt: string }) => {
     return (
